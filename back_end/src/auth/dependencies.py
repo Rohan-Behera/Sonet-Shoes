a9161434b2cba80c687d8576f685d3e645ff6049
Rohan Kumar Behera <rohan.behera@cozentus.com>
2026-06-09 16:13:25 +0530

    test

--- back_end/src/auth/dependencies.py
+++ back_end/src/auth/dependencies.py
@@ -47,4 +47,5 @@ async def get_current_user(token_details: TokenDetailsModel = Depends(AccessToke
     user = await user_service.get_user_by_email(email=user_email, session=session)
     if not user:
         raise UserNotFound()
+    
     return user
